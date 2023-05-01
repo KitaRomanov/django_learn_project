3ace1489c17cc5735ca228bde0eb8b8946486db4
Никита Ермишов <nikita321@bk.ru>
2023-05-01 18:34:21 +0300

    добавлена корзина

--- store/users/views.py
+++ store/users/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
 from django.urls import reverse
 
-from users.forms import User
+
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
+from products.models import Basket
 
 
 def login(request):
@@ -40,7 +41,8 @@ def profile(request):
         form = UserProfileForm(
             instance=request.user,
             data=request.POST,
-            files=request.FILES)
+            files=request.FILES,
+        )
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse('users:profile'))
@@ -48,7 +50,11 @@ def profile(request):
             print(form.errors)
     else:
         form = UserProfileForm(instance=request.user)
-    context = {'title': 'Store - Профиль,', 'form': form}
+
+    context = {'title': 'Store - Профиль,',
+               'form': form,
+               'baskets': Basket.objects.filter(user=request.user),
+               }
     return render(request, 'users/profile.html', context)
 
 
